a695fafc0937d07087eb86d1338cf251800687ad
Travis Martin <36611345+LincolnFleet@users.noreply.github.com>
2019-03-25 17:49:37 -0400

    Delete 02_create_inventories.rb

--- module-one-final-project-guidelines-atlanta-web-career-031119/db/migrate/02_create_inventories.rb
+++
@@ -1,8 +0,0 @@
-class Inventories < ActiveRecord::Migration[5.1]
-    def change
-        create_table :inventories do |t|
-            t.integer   :character_id
-            t.integer   :equipment_id
-        end
-    end
-end

f9bb0b0153ff20b5369a58056567e41c64092bbc
Travis Martin <36611345+LincolnFleet@users.noreply.github.com>
2019-03-25 17:49:10 -0400

    Delete environment.rb

--- module-one-final-project-guidelines-atlanta-web-career-031119/config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'bundler'
-require 'open-uri'
-require 'json'
-require 'net/http'
-require 'rake'
-Bundler.require
-
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
